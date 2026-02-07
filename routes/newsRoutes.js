8dd64a4a4c83fa0c497bcd135d512bfd1848f541
deedrop <155622024+deepakkushwaha9821@users.noreply.github.com>
2026-02-07 16:28:34 +0530

    Delete routes directory

--- routes/newsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createNews, getNews } = require("../controllers/newsController");
-
-router.post("/", createNews);
-router.get("/", getNews);
-
-module.exports = router;
