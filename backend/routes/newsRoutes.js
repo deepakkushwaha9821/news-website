1a6ea1a45e04fa2f0c2ac8b8b7e4d7ef4210e0de
deedrop <155622024+deepakkushwaha9821@users.noreply.github.com>
2026-02-10 07:25:02 +0530

    Add update and delete routes for news

--- backend/routes/newsRoutes.js
+++ backend/routes/newsRoutes.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const { createNews, getNews } = require("../controllers/newsController");
+const {
+  updateNews,
+  deleteNews
+} = require("../controllers/newsController");
 
 router.post("/", createNews);
 router.get("/", getNews);
+router.put("/:id", updateNews);      
+router.delete("/:id", deleteNews);
+
 
 module.exports = router;
